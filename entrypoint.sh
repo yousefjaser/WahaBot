fe6b77d67d98418b049d148db45246382653c6f3
devlikepro <waha@devlike.pro>
2025-01-10 09:02:43 +0700

    [core] Use UV_THREADPOOL_SIZE as cpu*1

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Calculate UV_THREADPOOL_SIZE based on number of CPUs
 cpus=$(node -e "const os = require('os'); console.log(os.cpus().length);")
-uv_threadpool_size=$(($cpus * 2))
+uv_threadpool_size=$(($cpus * 1))
 
 # Set UV_THREADPOOL_SIZE as an environment variable
 export UV_THREADPOOL_SIZE="${UV_THREADPOOL_SIZE:-$uv_threadpool_size}"
